790a953b133a8dfe503dcf442bafb5fa8904789d
Kenvickie <vickiekea8@gmail.com>
2024-04-20 16:18:26 +0100

    added dark mode to the home page

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,13 +1,28 @@
+// for the header slide
+
 let currentSlide = 1;
 
 const slides = document.querySelectorAll('.headImg');
 
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].style.animation = "none";
+// for the dark and light mode toggle 
 
-    void slides[currentSlide].offsetWidth;
-    slides[currentSlide].style.animation = null;
-} 
+const body = document.querySelector("body");
+const nav = document.querySelector("nav");
+const modeToggle = document.querySelector(".dark-light");
 
-setInterval(nextSlide, 5000);
+console.log(modeToggle);
+
+// function nextSlide() {
+//     currentSlide = (currentSlide + 1) % slides.length;
+//     slides[currentSlide].style.animation = "none";
+
+//     void slides[currentSlide].offsetWidth;
+//     slides[currentSlide].style.animation = null;
+// } 
+
+// setInterval(nextSlide, 5000);
+
+modeToggle.addEventListener("click", ()=> {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
+})
